a122852bf484847bc45360ffd2906d04075243b0
Quang Nguyen <quangnn@bravebits.vn>
2017-06-01 16:26:38 +0700

    First commit

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-function mapAccent(str) {
+function mapAccent( str ) {
 	str = str.toLowerCase();
-	str = str.replace(/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/g, 'a');
-	str = str.replace(/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/g, 'e');
-	str = str.replace(/(ì|í|ị|ỉ|ĩ)/g, 'i');
-	str = str.replace(/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/g, 'o');
-	str = str.replace(/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/g, 'u');
-	str = str.replace(/(ỳ|ý|ỵ|ỷ|ỹ)/g, 'y');
-	str = str.replace(/(đ)/g, 'd');
+	str = str.replace( /(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/g, 'a' );
+	str = str.replace( /(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/g, 'e' );
+	str = str.replace( /(ì|í|ị|ỉ|ĩ)/g, 'i' );
+	str = str.replace( /(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/g, 'o' );
+	str = str.replace( /(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/g, 'u' );
+	str = str.replace( /(ỳ|ý|ỵ|ỷ|ỹ)/g, 'y' );
+	str = str.replace( /(đ)/g, 'd' );
 	return str;
 }
 
-function removeAccent(str) {
-	return str.split('')
-		.map(function(char) {
+function removeAccent( str ) {
+	return str.split( '' )
+		.map( function( char ) {
 				var uppercase = char.toLowerCase() !== char;
-				char = mapAccent(char);
+				char = mapAccent( char );
 				return uppercase ? char.toUpperCase() : char;
 			},
-		).join('');
+		).join( '' );
 }
